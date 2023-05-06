c94231a30be069f568f1f0c3e154295f71f0d242
tadab <nguyendat9697@gmail.com>
2023-05-06 17:02:02 +0700

    fix name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,38 @@
-import React from "react"
-import LandingPage from "../components/LandingPage"
-import Layout from "../components/Layout"
+import React from 'react';
+import SubFooter from '../components/LandingPage/NewLanding/LamLaiCuocDoi/SubFooter';
+import SubHero from '../components/LandingPage/NewLanding/LamLaiCuocDoi/SubHero';
 
-function landing() {
+import SubMainNav from '../components/LandingPage/NewLanding/LamLaiCuocDoi/SubMainNav';
+import SubPartner from '../components/LandingPage/NewLanding/LamLaiCuocDoi/SubPartner';
+import SubPTE from '../components/LandingPage/NewLanding/LamLaiCuocDoi/SubPTE';
+import SubRoadMap from '../components/LandingPage/NewLanding/LamLaiCuocDoi/SubRoadMap';
+import SubToken from '../components/LandingPage/NewLanding/LamLaiCuocDoi/SubToken';
+import SubTribe from '../components/LandingPage/NewLanding/LamLaiCuocDoi/SubTribe';
+
+function remake(props) {
   return (
-    <Layout>
-      <LandingPage />
-    </Layout>
-  )
+    <div className='text-white' >
+      <SubMainNav />
+      <SubHero />
+      <SubTribe />
+      <div className="relative overflow-hidden long-bg"
+        style={{
+          backgroundImage: "url(/images/long-bg.png)",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <SubPTE />
+        <SubRoadMap />
+        <SubToken />
+        <SubPartner />
+        <SubFooter />
+
+
+      </div>
+    </div>
+  );
 }
 
-export default landing
+export default remake;
